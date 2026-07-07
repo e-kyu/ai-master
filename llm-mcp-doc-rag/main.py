fbce0ecd9da0130475aa711dfa3948e2d9cc90eb
leekyu <kyu940@gmail.com>
2026-07-08 01:04:37 +0900

    llm-mcp-doc-rag에서 4가지 전문상담 데이터 index처리하도록 수정

--- llm-mcp-doc-rag/main.py
+++ llm-mcp-doc-rag/main.py
@@ -272,10 +272,10 @@ if __name__ == "__main__":
     #agent_infos.append({"mode":"PpsProductsAgent", "name":"조달청 물품 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsProductsAgentGuide.zip;./llm-mcp-doc-rag/resource/agent/PpsProductsAgent.zip;"})
     #agent_infos.append({"mode":"PpsStockpilingAgent", "name":"조달청 비축 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsStockpilingAgentGuide.zip;./llm-mcp-doc-rag/resource/agent/PpsStockpilingAgent.zip;"})
     
-    #agent_infos.append({"mode":"PpsGeneralServiceAgent", "name":"조달청 일반용역 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsGeneralServiceAgent.zip;"})
-    #agent_infos.append({"mode":"PpsTechnicalServicesAgent", "name":"조달청 기술용역 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsTechnicalServicesAgent.zip;"})
+    agent_infos.append({"mode":"PpsGeneralServiceAgent", "name":"조달청 일반용역 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsGeneralServiceAgent.zip;"})
+    agent_infos.append({"mode":"PpsTechnicalServicesAgent", "name":"조달청 기술용역 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsTechnicalServicesAgent.zip;"})
     agent_infos.append({"mode":"PpsConstructionAgent", "name":"조달청 시설공사 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsConstructionAgent.zip;"})
-    #agent_infos.append({"mode":"PpsProductsAgent", "name":"조달청 물품 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsProductsAgent.zip;"})
+    agent_infos.append({"mode":"PpsProductsAgent", "name":"조달청 물품 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsProductsAgent.zip;"})
     agent_infos.append({"mode":"PpsStockpilingAgent", "name":"조달청 비축 상담", "resource":"./llm-mcp-doc-rag/resource/agent/PpsStockpilingAgent.zip;"})
     
     
